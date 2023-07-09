4dc11d071c82936c96655ba18b5bb42ce1ce89e6
mahima-b-k <79080035+mahima-b-k@users.noreply.github.com>
2023-07-09 15:06:06 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://gofood:Tanmahii2002%40@ac-k63pewz-shard-00-00.di3va5r.mongodb.net:27017,ac-k63pewz-shard-00-01.di3va5r.mongodb.net:27017,ac-k63pewz-shard-00-02.di3va5r.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-icu79j-shard-0&authSource=admin&retryWrites=true&w=majority'
+const mongoURI = 'mongodb://gofood:<PASSWORD>@ac-k63pewz-shard-00-00.di3va5r.mongodb.net:27017,ac-k63pewz-shard-00-01.di3va5r.mongodb.net:27017,ac-k63pewz-shard-00-02.di3va5r.mongodb.net:27017/<COLLECTION NAME>?ssl=true&replicaSet=atlas-icu79j-shard-0&authSource=admin&retryWrites=true&w=majority'
 const mongodb = async () => {
     await mongoose.connect(mongoURI, (err, result) => {
         if (err) console.log("___", err);
